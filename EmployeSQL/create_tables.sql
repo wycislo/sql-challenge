398aea6a44fe4620e731681b186cb304ce799905
wycislo <wycislo@yahoo.com>
2021-09-11 21:15:04 -0500

    create tables and constraints

---
+++ EmployeSQL/create_tables.sql
@@ -0,0 +1,138 @@
+
+
+DROP TABLE IF EXISTS TITLES;
+DROP TABLE IF EXISTS SALARIES;
+DROP TABLE IF EXISTS EMPLOYEES;
+DROP TABLE IF EXISTS DEPT_EMP;
+DROP TABLE IF EXISTS DEPT_MANAGER;
+DROP TABLE IF EXISTS DEPARTMENTS;
+
+
+CREATE TABLE TITLES
+(
+TITLE_ID VARCHAR(50) PRIMARY KEY,
+TITLE	VARCHAR(50)
+)
+;
+CREATE TABLE SALARIES
+(
+EMP_NO INTEGER PRIMARY KEY,
+SALARY	INTEGER
+)
+;
+
+CREATE TABLE EMPLOYEES
+(
+EMP_NO INTEGER PRIMARY KEY,
+EMP_TITLE_ID VARCHAR(50),
+BIRTH_DATE	DATE,
+FIRST_NAME VARCHAR(50),
+LAST_NAME VARCHAR(50),
+SEX VARCHAR(1),
+HIRE_DATE DATE
+)
+;
+CREATE TABLE DEPT_EMP
+(
+EMP_NO INTEGER,
+DEPT_NO VARCHAR (50)
+	
+)
+;
+
+CREATE TABLE DEPARTMENTS
+(
+DEPT_NO VARCHAR PRIMARY KEY,
+DEPT_NAME VARCHAR
+)
+;
+CREATE TABLE DEPT_MANAGER
+(
+EMP_NO INTEGER PRIMARY KEY,
+DEPT_NO VARCHAR(20)
+)
+;
+
+--  FOREIGN KEYS 
+
+-- EMPLOYEES.EMP_TITLE_ID => TITLES.TITLE_ID
+
+-- EMPLOYEES.EMP_NO => DEPT_EMP.EMP_NO
+
+-- DEPT_EMP.DEPT_NO => DEPARTMENTS => DETP_NO
+
+-- DEPARTMENTS.DEPT_NO => DEPT_MANAGER.DEPT_NO
+
+ALTER TABLE employees 
+  DROP CONSTRAINT IF EXISTS titles;
+  
+ALTER TABLE dept_manager 
+  DROP CONSTRAINT IF EXISTS department;
+  
+ALTER TABLE dept_emp 
+  DROP CONSTRAINT IF EXISTS department2;
+  ALTER TABLE dept_emp 
+  DROP CONSTRAINT IF EXISTS employee_number1;
+ALTER TABLE dept_manager 
+  DROP CONSTRAINT IF EXISTS employee_number2;
+  
+ALTER TABLE salaries 
+  DROP CONSTRAINT IF EXISTS employee_number3;
+
+ALTER TABLE departments 
+  DROP CONSTRAINT IF EXISTS employee_number3;
+
+ALTER TABLE employees 
+  DROP CONSTRAINT IF EXISTS fk_titles;
+  
+ALTER TABLE dept_manager 
+  DROP CONSTRAINT IF EXISTS fk_department;
+  
+ALTER TABLE dept_emp 
+  DROP CONSTRAINT IF EXISTS fk_department2;
+  ALTER TABLE dept_emp 
+  DROP CONSTRAINT IF EXISTS fk_employee_number1;
+  
+ALTER TABLE dept_manager 
+  DROP CONSTRAINT IF EXISTS fk_employee_number2;
+  
+ALTER TABLE salaries 
+  DROP CONSTRAINT IF EXISTS fk_employee_number3;
+
+ALTER TABLE "employees"
+ADD CONSTRAINT "fk_titles"
+FOREIGN KEY (emp_title_id)
+REFERENCES "titles" (title_id);
+
+ALTER TABLE "dept_manager"
+ADD CONSTRAINT "fk_department1"
+FOREIGN KEY (dept_no)
+REFERENCES "departments" (dept_no);
+-- departments to department employee relationship
+
+
+ALTER TABLE "dept_emp"
+ADD CONSTRAINT "fk_department2"
+FOREIGN KEY (dept_no)
+REFERENCES "departments" (dept_no);
+-- employees to department employee relationship
+  
+ALTER TABLE "dept_emp"
+ADD CONSTRAINT "fk_employee_number1"
+FOREIGN KEY (emp_no)
+REFERENCES "employees" (emp_no);
+
+-- employees to dept_manager relationship
+
+
+ALTER TABLE "dept_manager"
+ADD CONSTRAINT "fk_employee_number2"
+FOREIGN KEY (emp_no)
+REFERENCES "employees" (emp_no);
+  
+
+-- employees to salaries relationship
+ALTER TABLE "salaries"
+ADD CONSTRAINT "fk_employee_number3"
+FOREIGN KEY (emp_no)
+REFERENCES "employees" (emp_no);
--- EmployeSQL/sql_challenge.py
+++ EmployeSQL/sql_challenge.py
@@ -1,9 +1,9 @@
-# write python program to connect to postgres and load data to database
+# write python program to connect to postgres and load data to postgre database
 #
 # conda install psycopg2 - PostgresSQL database adapter for Python
 #
-# https://www.texaslottery.com/export/sites/lottery/Games/Lotto_Texas/Winning_Numbers/lottotexas.csv
-# Future - automate download and refresh numbers 
+#
+#
 # 
 #
 
@@ -25,65 +25,31 @@ DB_PASS = os.getenv('DB_PASS')
 
 # print(DB_PASS)
 
-conn = psycopg2.connect(dbname=DB_NAME, user = DB_USER, 
-password = DB_PASS, host = DB_HOST)
-
-sqlCreateTable = ("""create table lotto_stage (game_name char(15), draw_mon numeric(2,0),
-draw_day numeric(2,0), draw_year numeric(4,0),num1 numeric(2,0), 
-num2 numeric(2,0), num3 numeric(2,0),num4 numeric(2,0),num5 numeric(2,0),num6 numeric(2,0))""")
-
-cur = conn.cursor()
-cur.execute('drop table if exists lotto_stage')
-cur.execute(sqlCreateTable)
-conn.commit()
+departments_df = pd.read_csv('data/departments.csv')
+dept_df = pd.read_csv('data/dept_emp.csv')
+dept_manager_df = pd.read_csv('data/dept_manager.csv')
+employees_df = pd.read_csv('data/employees.csv')
+salaries_df = pd.read_csv('data/salaries.csv')
+titles_df = pd.read_csv('data/titles.csv')
 
 
+# # sqlachemy
 
+engine = create_engine(f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/company')
 
-tx_lotto_data = 'https://www.texaslottery.com/export/sites/lottery/Games/Lotto_Texas/Winning_Numbers/lottotexas.csv'
-
-cols = ['game_name','draw_mon','draw_day','draw_year','num1','num2','num3','num4','num5','num6']
-
-df = pd.read_csv(tx_lotto_data,names = cols)
-
-# sqlachemy
-
-engine = create_engine(f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/lotto')
-
-# load df to database
-df.to_sql('lotto_stage',engine,index=False,if_exists='replace')
-
-# create tx_lotto table and combine date into date column
-
-sqlCreateTable = ("""create table tx_lotto (game_date date, num1 numeric(2,0), 
-num2 numeric(2,0), num3 numeric(2,0),num4 numeric(2,0),num5 numeric(2,0),num6 numeric(2,0))""")
-
-cur = conn.cursor()
-cur.execute('drop table if exists tx_lotto')
-cur.execute(sqlCreateTable)
-conn.commit()
-
-cur.execute ('truncate table tx_lotto;')
-conn.commit()
-sql = f'''
-INSERT INTO tx_lotto 
-(game_date,num1,num2,num3,num4,num5,num6) 
-SELECT '{draw_year}-{draw_mon}-{draw_day}'
-,num1
-,num2
-,num3
-,num4
-,num5
-,num6 FROM lotto_stage
-'''
-sqlMergeData = 'insert into tx_lotto (game_date,num1,num2,num3,num4,num5,num6) \
-select format (''''%s-%s-%s'''', draw_year,draw_mon,draw_day)::date, \
-		num1,num2,num3,num4,num5,num6 from lotto_stage;'
-
-cur.execute(sqlMergeData)
-conn.commit()
-
-
-print ('connection successful')
-conn.close()
+# # load df to database
+departments_df.to_sql('departments',engine,index=False,if_exists='append')
+print('departments')
+dept_df.to_sql('dept_emp',engine,index=False,if_exists='append')
+print('dept_emp')
+dept_manager_df.to_sql('dept_manager',engine,index=False,if_exists='append')
+print('dept_manager')
+employees_df.to_sql('employees',engine,index=False,if_exists='append')
+print('employees')
+salaries_df.to_sql('salaries',engine,index=False,if_exists='append')
+print('salaries')
+titles_df.to_sql('titles',engine,index=False,if_exists='append')
+print('tities')
+dept_df.to_sql('dept_emp',engine,index=False,if_exists='append')
+print('dept_emp')
 
